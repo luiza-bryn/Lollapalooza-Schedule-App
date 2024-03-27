f38c7990baa6e2f2047d732acca25a16b0e2fefd
luizabryn <bryn.luiza@gmail.com>
2024-03-27 16:29:32 -0300

    fix

--- App.js
+++ App.js
@@ -20,18 +20,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
