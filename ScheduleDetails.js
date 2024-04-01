5f88289e491b62a9876f381b7279c6d9ab9fdc31
Luiza Bryn <luiza.bryn@tdbusiness.com.br>
2024-04-01 19:43:47 -0300

    Update ScheduleDetails.js

--- ScheduleDetails.js
+++ ScheduleDetails.js
@@ -1,85 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button, ActivityIndicator, FlatList, TouchableOpacity, Linking } from 'react-native';
-import jsonData from './assets/dados.json';
+import * as React from "react";
+import { Text, View, StyleSheet, Button, Linking } from "react-native";
+import { Platform } from "react-native";
+import { TouchableOpacity, Image } from "react-native";
 
-const ScheduleListScreen = ({ navigation }) => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState([]);
+export default function ScheduleListScreen({ route, navigation }) {
+  const { contact } = route.params;
 
-  useEffect(() => {
-    // Simule um atraso de 2 segundos para carregar os dados
-    setTimeout(() => {
-      setData(jsonData);
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  const handleGetDirections = (item) => {
-    const url = `https://www.google.com/maps/dir/?api=1&origin=Current Location&destination=${item.endereco_apresentacao}&travelmode=driving`;
+  const handleGetDirections = (contact) => {
+    const url = `https://www.google.com/maps/dir/?api=1&origin=Current Location&destination=${contact.endereco_apresentacao}&travelmode=driving`;
     Linking.openURL(url);
   };
 
-    const handleOpenPlaylist = (item) => {
-    Linking.openURL(item.link_playlist);
+  const handleOpenPlaylist = (contact) => {
+    Linking.openURL(contact.link_playlist);
   };
 
   return (
-    <>
-      {loading ? (
-        <View style={styles.loadingContainer}>
-          <ActivityIndicator size="large" color="#0000ff" />
-        </View>
-      ) : (
-        <View>
-          <FlatList
-            data={data}
-            renderItem={({ item }) => (
-                <View>
-                <Text style={styles.name}>{item.nome}</Text>
-                <TouchableOpacity onPress={() => handleGetDirections(item)}>
-                  <Text style={styles.endereco}>{item.endereco_apresentacao}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => handleOpenPlaylist(item)}>
-                  <Text style={styles.playlist}>{item.link_playlist}</Text>
-                </TouchableOpacity>
-                <Text style={styles.resto}>{item.link_site}</Text>
-                <Text style={styles.resto}>{item.valor_ingresso}</Text>
-                <Text style={styles.resto}>{item.link_compra_ingresso}</Text>
-                <Text style={styles.resto}>{item.telefone}</Text>
-              </View>
-            )}
-          />
-          <Button title="Voltar" onPress={() => navigation.navigate('Home')} />
-        </View>
-      )}
-    </>
-  );
-};
+    <View>
+      <View>
+        <Text style={styles.name}>{contact.nome}</Text>
 
-export default ScheduleListScreen;
+        <TouchableOpacity onPress={() => handleGetDirections(contact)}>
+          <Text style={styles.endereco}>{contact.endereco_apresentacao}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => handleOpenPlaylist(contact)}>
+          <Text style={styles.playlist}>{contact.link_playlist}</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.resto}>{contact.link_site}</Text>
+
+        <Text style={styles.resto}>{contact.valor_ingresso}</Text>
+
+        <Text style={styles.resto}>{contact.link_compra_ingresso}</Text>
+
+        <Text style={styles.resto}>{contact.telefone}</Text>
+      </View>
+      <View>
+        <Button
+          title="Voltar"
+          onPress={() => navigation.navigate("ScheduleList")}
+        />
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    padding: 15
+    padding: 15,
   },
   name: {
     fontSize: 25,
     height: 44,
-    color: 'red'
+    color: "red",
   },
   resto: {
     fontSize: 18,
     height: 44,
-    color: 'blue'
+    color: "blue",
   },
   endereco: {
     fontSize: 18,
     height: 44,
-    color: 'yellow'
+    color: "yellow",
   },
   playlist: {
     fontSize: 18,
     height: 44,
-    color: 'green'
-  }
+    color: "green",
+  },
 });
