c53da816585e46cae8b25e864ec82e22f74b5b78
luizabryn <bryn.luiza@gmail.com>
2024-04-02 00:17:03 -0300

    Update FavoritesList.js
    Navegação para Details em FavoriteScreen

--- FavoritesList.js
+++ FavoritesList.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { TouchableOpacity, Image } from "react-native";
 
-export default function FavoritesListScreen({ route }) {
+export default function FavoritesListScreen({ navigation ,route }) {
   const { favorites } = route.params;
 
   return (
@@ -11,12 +12,14 @@ export default function FavoritesListScreen({ route }) {
         data={favorites}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate('ScheduleDetails', { contact: item })}>
           <View style={styles.showContainer}>
             <Text style={styles.showName}>{item.nome}</Text>
             <Text>Data: {item.data}</Text>
             <Text>Horário: {item.horario}</Text>
             <Text>Local: {item.endereco_apresentacao}</Text>
           </View>
+          </TouchableOpacity> 
         )}
       />
     </View>
